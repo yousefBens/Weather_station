6ad03cbfdf51f907672c9bf7c4af9036eff5a8be
Yousef <77381634+yousefBens@users.noreply.github.com>
2023-12-12 17:30:35 +0100

    Delete Files

--- Debug/Drivers/HTS221/hts221_reg.d
+++
@@ -1,3 +0,0 @@
-Drivers/HTS221/hts221_reg.o: ../Drivers/HTS221/hts221_reg.c \
- ../Drivers/HTS221/hts221_reg.h
-../Drivers/HTS221/hts221_reg.h:
--- Debug/Drivers/LPS22HH/lps22hh_reg.d
+++
@@ -1,3 +0,0 @@
-Drivers/LPS22HH/lps22hh_reg.o: ../Drivers/LPS22HH/lps22hh_reg.c \
- ../Drivers/LPS22HH/lps22hh_reg.h
-../Drivers/LPS22HH/lps22hh_reg.h:
